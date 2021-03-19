d75a75f9d9c6c66ee63c25fae530157015d41865
joshsrai <joshsrai@gmail.com>
2021-03-19 19:48:44 -0400

    renamed to splash from myRomour

---
+++ src/components/splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheet/splash.css';
+export default function myRumours() {
+  return (
+<div class="welcome">
+<div id = "quote"> Welcome to my Blog
+
+  <p>Welcome <span class="redText">to</span> my blog.</p>
+  </div>
+    </div>
+  )
+}
