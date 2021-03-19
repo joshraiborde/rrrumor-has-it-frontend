11af4fa1f83a8f9271cda191ed4f8c30be7e8d3b
joshsrai <joshsrai@gmail.com>
2021-03-19 08:21:47 -0400

    added className to container

--- src/components/postRumour.js
+++ src/components/postRumour.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function postRumours() {
   return (
-  <div class="container"><h2>Post Rumour here</h2>
+  <div className="container"><h2>Post Rumour here</h2>
   </div>);
 }
