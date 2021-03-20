077a78377e2796f23c0478271c0cd0ed0581a764
joshsrai <joshsrai@gmail.com>
2021-03-20 13:58:29 -0400

    imported singleReducer, added single singleReducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'
 import userReducer from './usersReducer'
+import singleReducer from './singleReducer'
 
 export default combineReducers({
-  users: userReducer
+
+  users: userReducer,
+  singles: singleReducer,
 })
