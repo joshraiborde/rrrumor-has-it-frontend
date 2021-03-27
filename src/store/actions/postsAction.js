ead2700e6fca426254b2bc0f54eb5ba9b372f613
joshsrai <joshsrai@gmail.com>
2021-03-27 14:49:23 -0400

    reformatted layout

--- src/store/actions/postsAction.js
+++ src/store/actions/postsAction.js
@@ -1,52 +1,31 @@
 import {GET_POSTS} from '../types'
 import axios from 'axios'
 
-
-
-
 export const getPosts = () => async dispatch => {
-
-
-        const res = await axios.get(`http://localhost:3000/posts`)
-        dispatch( {
+    const res = await axios.get(`http://localhost:3000/posts`)
+        dispatch({
             type:GET_POSTS,
             payload: res.data
         })
-
-
-
 }
+
 export const getSinglePosts = (id) => async dispatch => {
-
-
     const res = await axios.get(`http://localhost:3000/get/post?id=`+id)
     dispatch( {
         type:'GET_SINGLE_POST',
         payload: res.data
     })
-
-
-
 }
 
-
-
-
-
 export const deletePosts = (id) => async dispatch => {
-
-
     const res = await axios.delete(`http://localhost:3000/posts/`+id)
-    dispatch( {
+    dispatch({
         type:'DELETE_POSTS',
         payload: res.data
     })
-
-
-
 }
-export const createPosts = (content) => async dispatch => {
 
+export const createPosts = (content) => async dispatch => {
     const postContent = { content: content };
     await axios.post(`http://localhost:3000/posts`,postContent).then((res)=>{
         dispatch( {
@@ -54,18 +33,12 @@ export const createPosts = (content) => async dispatch => {
             payload: res.data
         })
     })
-    .then(()=> {
+    .then(() => {
         window.location = "/home";
     })
-
-
-
-
-
-
 }
-export const updatePosts = (id,content) => async dispatch => {
 
+export const updatePosts = (id,content) => async dispatch => {
     const postContent = { content: content };
     await axios.put(`http://localhost:3000/posts/`+id,postContent).then((res)=>{
         dispatch( {
@@ -73,17 +46,9 @@ export const updatePosts = (id,content) => async dispatch => {
             payload: res.data
         })
     })
-
-
-
-
-
-
-
-
 }
-export const createComments = (id,content) => async dispatch => {
 
+export const createComments = (id,content) => async dispatch => {
     const postContent = { content: content,post_id:id };
     await axios.post(`http://localhost:3000/comments`,postContent).then((res)=>{
         dispatch( {
