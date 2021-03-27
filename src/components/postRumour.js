fae7294cbe640adb7748789c7412a5a986e68fb3
joshsrai <joshsrai@gmail.com>
2021-03-27 14:48:03 -0400

    reformatted layout, renamed labels

--- src/components/postRumour.js
+++ src/components/postRumour.js
@@ -8,36 +8,33 @@ class PostRumour extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "Enter a Rumour here..."
+      value: 'Please Enter a Rumour'
     };
     this.handleChange = this.handleChange.bind(this);
 
-
-
     this.createPostsFunction = this.createPostsFunction.bind(this);
   }
-  handleChange(event) {
 
+  handleChange(event) {
   this.setState({value: event.target.value});
   }
+
   createPostsFunction(content){
     this.props.createPosts(content);
-
   }
+
   render() {
    return(<div>
-   <h1 className="view-posts">Post a Rumour</h1>
-
-   <div className="paper">
-    <div className="paper-content">
+    <h1 className="view-posts">Post a JUICY Rumour!</h1>
+     <div className="paper">
+       <div className="paper-content">
         <textarea placeholder="Enter Rumour Here..."
-        onChange={this.handleChange} autoFocus/>
+          onChange={this.handleChange} autoFocus/>
+      </div>
     </div>
-</div>
      <br/>
-
-
-     <button onClick={() => this.createPostsFunction(this.state.value)} className="view-comments">Create Rumour</button>
+     <button onClick={() => this.createPostsFunction(this.state.value)}
+        className="view-comments">Post Rumour</button>
    <br/>
    <br/>
    <br/>
