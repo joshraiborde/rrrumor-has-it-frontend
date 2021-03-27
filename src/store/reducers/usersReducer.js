dab9b984ebd7f4e1123b2c8a16ca635145a26378
joshsrai <joshsrai@gmail.com>
2021-03-27 14:51:00 -0400

    reformatted layout

--- src/store/reducers/usersReducer.js
+++ src/store/reducers/usersReducer.js
@@ -6,40 +6,26 @@ const initialState = {
 }
 // eslint-disable-next-line
 export default function(state = initialState, action){
-
     switch(action.type){
-
         case GET_POSTS:
-        return {
-            ...state,
-            posts:action.payload,
-
-
-        }
+            return {
+                ...state,
+                posts:action.payload,
+            }
         case 'DELETE_POSTS':
-        return {
-            ...state,
-            posts:action.payload,
-
-
-        }
+            return {
+                ...state,
+                posts:action.payload,
+            }
         case 'CREATE_POSTS':
             return {
                 ...state,
                 posts:action.payload,
-
-
-
             }
-            case 'UPDATE_POSTS':
-                return {
-                    ...state,
-                    posts:action.payload,
-
-
-                }
- 
-        default: return state
-    }
-
-}
+        case 'UPDATE_POSTS':
+            return {
+                ...state,
+                posts:action.payload,
+            }
+            default: return state
+        }}
