d168ab990d80979860c2b9d11bf4cb0ec1ba4bdf
joshsrai <joshsrai@gmail.com>
2021-03-27 14:42:57 -0400

    deleted empty spaces, renamed labels, added route, formatted layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import './stylesheet/navbar.css'
 import React from 'react';
@@ -8,40 +7,23 @@ import PostRumour from './components/postRumour.js';
 import Splash from './components/splash.js';
 import Comments from './components/postComment';
 
-
-
 function App() {
   return (
-    <BrowserRouter>
+  <BrowserRouter>
     <nav>
-    <div className="menu-icon">
-<span className="fas fa-bars"></span></div>
-<div className="logo">
-Rumour Has It...</div>
-<div class="nav-items">
-
-<li><Link to="/home">Home</Link></li>
-<li><Link to="/post/rumour">Post Rumour</Link></li>
-
-
-</div>
-
-
-</nav>
- <Route exact path="/" component={Splash}/>
-<Switch>
-          <Route path="/home">
-<Home/>
-          </Route>
-          <Route path="/post/rumour">
-<PostRumour/>
-          </Route>
-          <Route path="/comments">
-<Comments/>
-          </Route>
-
-
-
+      <div className="menu-icon"> <span className="fas fa-bars"></span></div>
+          <div className="logo">Rumour Has It...</div>
+              <div class="nav-items">
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/post/rumour">Post Rumour</Link></li>
+                <li><Link to="/comments">Rumour Faqs</Link></li>
+                </div>
+                </nav>
+      <Route exact path="/" component={Splash}/>
+        <Switch>
+          <Route path="/home" render={(props) => <Home {...props} key={Date.now()}/>}>
+          </Route><Route path="/post/rumour"><PostRumour/></Route>
+          <Route path="/comments"><Comments/></Route>
         </Switch>
       </BrowserRouter>
   );
