7d4b6b4e7dcebeecd394f9d71178ddd3ad36025b
joshsrai <joshsrai@gmail.com>
2021-03-27 14:47:02 -0400

    reformatted layout

--- src/components/postComment.js
+++ src/components/postComment.js
@@ -17,47 +17,53 @@ class PostComment extends React.Component {
         );
         this.handleChange = this.handleChange.bind(this);
         this.PostComment = this.PostComment.bind(this);
-
-
+        this.PostCommentEnter = this.PostCommentEnter.bind(this);
     }
+
     handleChange(event) {
         this.setState({value:event.target.value})
     }
+
     PostComment(id,value){
+       console.log("clicked");
         this.props.createComments(id,value);
     }
-    componentDidMount() {
 
-        this.props.getSinglePosts(this.props.id);
-
-
-
-     /*   axios.get(`http://localhost:3000/posts`)
-        .then(res => {
-            const posts = res.data;
-            console.log(posts);
-            this.setState({ posts });
-        })
-        */
+    PostCommentEnter(e,id,value){
+        if(e.key==="Enter"){
+            this.props.createComments(id,value);
+        }
     }
+
+    componentDidMount() {
+        this.props.getSinglePosts(this.props.id);
+    }
+
     render() {
-
-
-
          const {posts} = this.props.singles
          console.log(posts);
-
-
          if(posts.data){
+             return (
+             <div>
+                <div>
+                    <div>
+                        <div className="card">
+                            <div className="card-body">
+                            {/* <h1 className="post-id">Post Id:{posts.data.id}</h1> */}
+                                <h4 className="view-content">Rumour: {posts.data.attributes.content}</h4>
+                                <input  type="text" placeholder="Comment Here"
+                                    onChange={this.handleChange.bind()}
+                                    onKeyPress={(e) => this.PostCommentEnter(e,posts.data.id,this.state.value)}
+                                    value={this.state.value} class="cool-one valid"></input>
+                                <br/>
+                                <button onClick={() => this.PostComment(posts.data.id,this.state.value)}
+                                        className="delete-comments">Posts A comment</button>
+                                </div>
+                        </div>
 
-      return (<div>
-     <div> <div > <div  className="card"> <div className="card-body">
-          {/* <h1 className="post-id">Post Id:{posts.data.id}</h1> */}
-          <input  type="text" placeholder="Please write a Comment"
-        onChange={this.handleChange.bind()}  value={this.state.value} class="cool-one valid"></input>
-          <h4 className="view-content">Rumour: {posts.data.attributes.content}</h4> <br/>
-           <button onClick={() => this.PostComment(posts.data.id,this.state.value)}  className="delete-comments">Posts A Comment</button> </div> </div>
-            <br/> </div> </div>
+                        <br/>
+                    </div>
+                </div>
             <br/>
             <br/>
 
@@ -66,20 +72,21 @@ class PostComment extends React.Component {
                 <div>
                 <div class="card">
                 <div className="card-body">
-
                 <h4 className="view-content">{animal.content}</h4> <br/>
                   </div>
                   </div>
                   <br/>
                   </div>
 
-      )): <h1 className="no-comments">NO COMMENTS</h1>}
-
+      )):
+      <h1 className="no-comments">NO COMMENTS</h1>}
 
       </div>);
          }
          else{
-             return(<h1>Loading</h1>)
+             return(
+             <h1>Don't start Rumours, it's bad for you...</h1>   
+                )
          }
     }
   }
