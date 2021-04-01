baca1e291d4bae9c418d6bb44c22574698f27839
joshsrai <joshsrai@gmail.com>
2021-04-01 18:27:35 -0400

    added About Us, Contact Us links and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import './stylesheet/navbar.css'
+import './stylesheet/footer.css'
 import React from 'react';
 import { BrowserRouter,Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home.js';
 import PostRumour from './components/PostRumour.js';
 import Splash from './components/Splash.js';
 import Comments from './components/PostComment';
+import About from './components/AboutUs';
+import Footer from './components/footer';
+import Contact from './components/ContactUs';
 
 function App() {
   return (
@@ -16,7 +20,9 @@ function App() {
               <div class="nav-items">
                 <li><Link to="/home">Home</Link></li>
                 <li><Link to="/post/rumour">Post Rumour</Link></li>
-                <li><Link to="/comments">Rumour Faqs</Link></li>
+                <li><Link to="/about/us">About us</Link></li>
+                <li><Link to="/contact/us">Contact us</Link></li>
+
                 </div>
                 </nav>
       <Route exact path="/" component={Splash}/>
@@ -24,7 +30,14 @@ function App() {
           <Route path="/home" render={(props) => <Home {...props} key={Date.now()}/>}></Route>
           <Route path="/post/rumour"><PostRumour/></Route>
           <Route path="/comments"><Comments/></Route>
+          <Route path="/about/us"><About/></Route>
+          <Route path="/contact/us"><Contact/></Route>
         </Switch>
+        <Footer/>
+        <section>
+
+</section>
+
       </BrowserRouter>
   );
 }
